892340dddcde8c8dae685ec6717b4899a91edff9
Charishma Buchireddy <177282494+charishmab27@users.noreply.github.com>
2026-04-23 18:40:24 -0400

    Update trips_controller.rb

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,24 +1,51 @@
 class TripsController < ApplicationController
   before_action :require_login
-  
+  before_action :set_trip, only: [:show, :edit, :update, :destroy]
+
   def index
+    @trips = current_user.trips
   end
 
   def show
   end
 
   def new
+    @trip = current_user.trips.build
   end
 
   def create
+    @trip = current_user.trips.build(trip_params)
+
+    if @trip.save
+      redirect_to @trip, notice: "Trip created successfully."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    if @trip.update(trip_params)
+      redirect_to @trip, notice: "Trip updated successfully."
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @trip.destroy
+    redirect_to trips_path, notice: "Trip deleted successfully."
+  end
+
+  private
+
+  def set_trip
+    @trip = current_user.trips.find(params[:id])
+  end
+
+  def trip_params
+    params.require(:trip).permit(:name, :start_date, :end_date)
   end
 end
