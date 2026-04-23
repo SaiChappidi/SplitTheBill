a70edc2db9d7bbe2c8a4ad0224aedf4073ca4573
Charishma Buchireddy <177282494+charishmab27@users.noreply.github.com>
2026-04-23 18:39:49 -0400

    Update trip.rb

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,3 +1,8 @@
 class Trip < ApplicationRecord
   belongs_to :user
+  has_many :participants, dependent: :destroy
+  has_many :expenses, dependent: :destroy
+  validates :name, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 end
